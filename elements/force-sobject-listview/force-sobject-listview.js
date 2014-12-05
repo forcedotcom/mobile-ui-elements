07275435d8a45e1b61f24e190e8e6f9014ca3508
Harshit Pandey <mailtoharshit@gmail.com>
2014-12-04 17:19:43 -0800

    Added Listview

---
+++ elements/force-sobject-listview/force-sobject-listview.js
@@ -0,0 +1,28 @@
+<script>
+var query='';
+  // Call the List View API to get the results (also includes column definitions)
+    Polymer('force-ui-listview', {
+     observe: {
+      sobject: 'ready'
+      },
+     ready: function() { 
+      this.query='';
+      this.query = '/services/data/v32.0/sobjects/' + this.sobject + '/listviews/';
+      },
+     updateListView: function() { 
+       this.sobject=sobject;
+     }
+  }); 
+   $.ajax({
+      url : query, 
+      headers : { 'Authorization' : 'Bearer {!$Api.Session_ID}' },
+      datatype : 'json', 
+      success : function(response) {                   
+        // Clear current List View info
+        $('#listview').empty();
+        $.each(response.listviews, function(index, column) {
+          $('#listview').append($('<li>' + column.label + '</li>'));
+        });
+     }
+  }); 
+</script>
