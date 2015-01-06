28785d76bbd5b693fce9795d51d42f7c1fc718c6
Harshit Pandey <mailtoharshit@gmail.com>
2015-01-06 13:26:42 -0800

    using forcetkclient for the list view API call

--- elements/force-sobject-listview/force-sobject-listview.js
+++ elements/force-sobject-listview/force-sobject-listview.js
@@ -1,26 +1,18 @@
-var query='';
-  // Call the List View API to get the results (also includes column definitions)
-    Polymer('force-ui-listview', {
-     observe: {
-      sobject: 'ready'
-      },
-     ready: function() { 
-      this.query='';
-      this.query = '/services/data/v32.0/sobjects/' + this.sobject + '/listviews/';
-      },
-     updateListView: function() { 
-       this.sobject=sobject;
-     }
-  }); 
-   $.ajax({
-      url : query, 
-      headers : { 'Authorization' : 'Bearer {!$Api.Session_ID}' },
-      datatype : 'json', 
-      success : function(response) {                   
-        // Clear current List View info
-        $('#listview').empty();
-        $.each(response.listviews, function(index, column) {
-          $('#listview').append($('<li>' + column.label + '</li>'));
-        });
-     }
-  }); 
+// Call the List View API to get the results (also includes column definitions)
+Polymer('force-sobject-listview', {
+  observe: {
+    sobject: 'updateListView'
+  },
+  updateListView: function() {
+    var that = this;
+    that.listviews = null;
+    var query = '/services/data/v32.0/sobjects/' + this.sobject + '/listviews/';
+    var success = function(response) {                   
+      that.listviews = response.listviews;
+      console.log(that.listviews);
+    };
+    SFDC.launcher.then(function(){
+         Force.forcetkClient.impl.ajax(query, success);
+    });
+  }
+});
