a82d0d30ca5735a5b9edc2da56abc20f683d2edf
Kunal Chitalia <kchitalia@salesforce.com>
2016-01-28 00:37:35 -0800

    Add changes lost in clean up

--- elements/force-sobject-collection/force-sobject-collection.js
+++ elements/force-sobject-collection/force-sobject-collection.js
@@ -136,7 +136,7 @@
             }
         }, viewProps),
         observers: [
-            'reset(sobject, query, querytype, collection)'
+            'reset(sobject, query, querytype)'
         ],
         
         /**
@@ -144,69 +144,36 @@
          *
          * @method reset
          */
-        reset: function(sobject, query, querytype, collection) {
+        reset: function(sobject, query, querytype) {
             this.collection.config = generateConfig(_.pick(this, _.keys(viewProps)));
             this.collection.reset();
-            this.resetCount++;
             if (this.autosync) this.fetch();
         },
-        
-        /**
-         * Initiates the fetching of more records if there's an available cursor and the collection size is less than maxsize.
-         *
-         * @method fetchMore
-         */
-        fetchMore: function() {
-            var that = this;
-            if ((this.maxsize < 0 || this.maxsize > this.collection.size())
-                && this.collection.hasMore()) 
-                return this.collection.getMore()
-                    .then(function() {
-                        that.fire('sync');
-                    });
-        },
-
         /**
          * Initiates the fetching of records from the relevant data store (server/offline store).
          *
          * @method fetch
          */
         fetch: function() {
-            var collection = this.collection;
-            var resetCount = this.resetCount; // captured for closure below
-
             var onFetch = function() {
-                if (this.resetCount > resetCount) {
-                    // This is an old query -- ignore
-                    return;
-                }
-
-                if (collection.length == 0) {
-                    // Nothing came back 
-                    return;
-                }
+                if ((this.maxsize < 0 || this.maxsize > this.collection.length)
+                    && this.collection.hasMore())
+                    this.collection.getMore().then(onFetch);
             }.bind(this);
 
             var operation = function() {
-                var that = this;
-                
+                var collection = this.collection;
+                var store = this.$.store;
+
                 if (collection.config) {
                     // Define the collection model type. Set the idAttribute to 'ExternalId' if sobject is external object.
                     collection.model = Force.SObject.extend({
-                        idAttribute: that.sobject.toLowerCase().search(/__x$/) > 0 ? 'ExternalId' : 'Id'
+                        idAttribute: this.sobject.toLowerCase().search(/__x$/) > 0 ? 'ExternalId' : 'Id'
                     });
-                    
-                    var promise = Promise.resolve(SFDC.launcher)
-                        .then(function() {
-                            if (that.cachePromise) {
-                                return that.cachePromise.then(function(store) {
-                                    collection.cache = store.cache;
-                                    collection.cacheForOriginals = store.cacheForOriginals;
-                                });
-                            }
-                        });
-
-                    promise.then(function() {
+                    $.when(store.cacheReady, SFDC.launcher)
+                    .done(function() {
+                        collection.cache = store.cache;
+                        collection.cacheForOriginals = store.cacheForOriginals;
                         collection.fetch({ reset: true, success: onFetch });
                     });
                 }
